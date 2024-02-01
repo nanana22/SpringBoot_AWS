7db14e261f821b478a5fa3c878fc218f58f7b323
MINJI <karadi@naver.com>
2024-02-01 22:27:28 +0900

    Posts컨트롤러, 서비스, Dto, 테스트코드 작성
    Signed-off-by: MINJI <karadi@naver.com>

---
+++ src/main/java/com/springboot/aws/project/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.springboot.aws.project.service.posts;
+
+import com.springboot.aws.project.domain.posts.PostsRepository;
+import com.springboot.aws.project.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
